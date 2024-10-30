2c9cdbfca436fc1f8590cd9701a0426714c58c03
Sai Rama Raju Puram <sairamrajpuram2002@gmail.com>
2024-10-30 17:11:23 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Puram Sai Rama Raju' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
